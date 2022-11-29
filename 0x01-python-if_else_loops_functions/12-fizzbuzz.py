5da2ea3e086edb84882be7cbf357836532d734e7
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-11-29 13:32:46 -0800

    a function that prints the numbers from 1 to 100 separated by a space Fizz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 5 == 0 and num % 3 == 0:
+            print('FizzBuzz', end=' ')
+        elif num % 3 == 0:
+            print('Fizz', end=' ')
+        elif num % 5 == 0:
+            if num == 100:
+                print('Buzz', end=' ')
+            else:
+                print('Buzz', end=' ')
+        else:
+            print('{:d}'.format(num), end=' ')
+
+
+if __name__ == '__main__':
+    fizzbuzz()
