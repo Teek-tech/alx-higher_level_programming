1a38bd09186380b65bc890bbf01db76c080fce23
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-03 12:12:18 -0800

    a program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    total_args = len(argv)
+    sum = 0
+    for i in range(1, total_args):
+        sum += int(argv[i])
+        
+    print(sum)
