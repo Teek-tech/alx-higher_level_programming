aadd3121970c4c7b2574a417d9b2476b8bbf4d79
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-03 11:16:40 -0800

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
 
     if len(argv) == 1:
         print("0 arguments.")
+    elif len(argv) == 2:
+        print("{:d} arguments:".format(len(argv) - 1))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
 
