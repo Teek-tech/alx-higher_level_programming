1f6a9962120ef00a34a01e5c989d4593a3211ffe
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-05 15:03:41 -0800

    fix error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    total_args = len(argv)
-    sum = 0
-    for i in range(1, total_args):
-        sum += int(argv[i])
-        print(sum)
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
