80aeff1376059b6f2dc17ef27dd4342d863c7ebc
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-05 14:49:15 -0800

    a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,18 +1,18 @@
-#ifdef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
- * struct listsint_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Description: singy linked list niode structure
+ * Description: singly linked list node structure
  * for project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
