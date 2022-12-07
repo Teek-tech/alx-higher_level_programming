edecfaa20a049ef4c57171d64c2b3f33848b86f8
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-07 15:17:36 -0800

    fix error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-        new_dict = dict(a_dictionary)
+    new_dict = dict(a_dictionary)
         for k, v in new_dict.items():
             if v == value:
                 del a_dictionary[k]
