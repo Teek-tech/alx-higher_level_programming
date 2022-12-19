fd71c611f8d6713cb0e8ea23b76fbc6c4ced1c77
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-19 13:51:26 -0800

    fix eror

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(0)
                 print("out of range")
     finally:
-        return 
+        return result 
